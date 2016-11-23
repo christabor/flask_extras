36740bf92999dae7258e1aeb6e8d82b43e1773f5
christaborium <dxdstudio@gmail.com>
2016-11-22 21:37:15 -0800

    Add config and plugin wrapper for app test fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 from flask import Flask
 import pytest
 
+from flask_extras import FlaskExtras
 
 app = Flask('__config_test')
+app.secret_key = '123'
+app.debug = True
+FlaskExtras(app)
 
 
 @pytest.fixture()
