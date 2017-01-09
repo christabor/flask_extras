8a15b1ea40c34490bb94f214ccd73a4f682a90be
christaborium <dxdstudio@gmail.com>
2017-01-09 13:44:16 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def readme():
 
 setup(
     name='flask_extras',
-    version='4.0.0',
+    version='4.0.1',
     description=('Assorted useful flask views, blueprints, '
                  'Jinja2 template filters, and templates/macros'),
     long_description=readme(),
